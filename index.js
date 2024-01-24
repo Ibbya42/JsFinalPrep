385ae780fbe5e6c25d57c8f30cdc259e0f81b6f5
ibbya42 <ibbyali4214@gmail.com>
2024-01-24 13:42:08 +1300

    24/01 First Push

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+// Async function to call the user data from the api 
+// Create a function called main 
+async function main(){
+    // Create a const for users will collect the information from the back end. 
+    const users = await fetch("https://jsonplaceholder.typicode.com/users")
+    // Create a const that will convert the backend data to frontend data. 
+    const usersData = await users.json();
+    console.log(usersData);
+}
+
+main();
