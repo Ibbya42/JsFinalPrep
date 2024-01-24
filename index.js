ac617c07d101533ac73a68aec7fea50ac8bc3bc3
ibbya42 <ibbyali4214@gmail.com>
2024-01-24 14:56:23 +1300

    24/01 Final Push

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ async function main(){
     const users = await fetch("https://jsonplaceholder.typicode.com/users")
     // Create a const that will convert the backend data to frontend data. 
     const usersData = await users.json();
-    console.log(usersData);
+    // Creating a const that will call the html document through query selector
+    const userListEl = document.querySelector('.user-list');
+    // Set the innerHtml to the element data which we mapped to recieved all the data
+    // from the api in the styled format 
+    userListEl.innerHTML = usersData.map((user) => `<div class="user-card">
+    <div class="user-card__container">
+        <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="${user.website}" target="_blank">
+        ${user.website}</a></p>
+    </div>
+    </div>`).join("");
+   
 }
 
 main();
