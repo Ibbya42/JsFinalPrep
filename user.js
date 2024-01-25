2f2cad7bddc9632a6dd42c34ea7be548bfe7653e
ibbya42 <ibbyali4214@gmail.com>
2024-01-26 12:24:00 +1300

    26/01 Final Push

--- user.js
+++ user.js
@@ -1,45 +1,38 @@
 // Creating a const that will call the html document div through query selector
 const postListEL = document.querySelector('.post-list')
+ // Get the item id from the local storage 
+ const id = localStorage.getItem("id");
 
 // Creating a function to search for different id through the search bar
 async function onSearchChange(event){
     const id = event.target.value;
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postData = await posts.json();
-
-    postListEL.innerHTML = postData.map ( post => `
-    <div class="post">
-      <div class="post__title">
-        ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>`).join('');
+    renderPosts(id)
 
 
 }
 
-async function main(){
-    // Get the item id from the local storage 
-    const id = localStorage.getItem("id");
+async function renderPosts(id){
     // Create a const posts that will call the api from the backend 
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     // Create another const that calls the backend data and converts it into 
-    // frontend reable code. 
+    // frontend readable code. 
     const postData = await posts.json();
-    console.log(postData)
-
     // Using Array Map to add the backend data into Html 
-   postListEL.innerHTML = postData.map ( post => `
-    <div class="post">
-      <div class="post__title">
-        ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>`).join('');
+    // Calling the function that has the div content (posthtml)
+   postListEL.innerHTML = postData.map ( post => postHTML(post)).join('');
+    
 }
 
-main();
+// Creating a function that will store the div in the Html file and can collab with the api
+function postHTML(post){
+    return ` <div class="post">
+    <div class="post__title">
+      ${post.title}
+    </div>
+    <p class="post__body">
+      ${post.body}
+    </p>
+  </div>`
+}
+
+renderPosts(id);
