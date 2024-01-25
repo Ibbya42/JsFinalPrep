add0561c1101babb252d25ff2b2e1edfb4d936ff
ibbya42 <ibbyali4214@gmail.com>
2024-01-26 11:16:46 +1300

    26/01 Second Push

--- user.js
+++ user.js
@@ -1,6 +1,25 @@
 // Creating a const that will call the html document div through query selector
 const postListEL = document.querySelector('.post-list')
 
+// Creating a function to search for different id through the search bar
+async function onSearchChange(event){
+    const id = event.target.value;
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postData = await posts.json();
+
+    postListEL.innerHTML = postData.map ( post => `
+    <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`).join('');
+
+
+}
+
 async function main(){
     // Get the item id from the local storage 
     const id = localStorage.getItem("id");
